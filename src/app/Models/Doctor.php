ce3b9b549cc81fd258130e603751d8c574614032
Luiz Felipe <lfelipelopesti@gmail.com>
2025-08-21 19:47:19 -0300

    feat: initial commit with database structure

---
+++ src/app/Models/Doctor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Doctor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'speciality'
+    ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ src/database/factories/PatientFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'address' => fake()->streetAddress(),
+            'city' => fake()->city(),
+            'state' => fake()->state,
+            'zipcode' => fake()->postcode(),
+            'country'=> fake()->country(),
+        ];
+    }
+}
